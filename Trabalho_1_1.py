7f8b27a04b2f5fe4bd470de844f48ba1d7f6dcf2
Lucas Macena <lucasmacenal45@gmail.com>
2021-06-30 13:15:43 -0300

    Tópicos 1, 2, 4, 5 e 6 completos

--- Trabalho_1_1.py
+++ Trabalho_1_1.py
@@ -1,6 +1,7 @@
 from uteis import *
 from Trabalho_uteis import *
 
+
 '''Definição dos diretórios de arquivos'''
 path = os.path.abspath('')
 path_clientes = os.path.join(path,'database','dados_clientes.xlsx')
@@ -22,10 +23,15 @@ plan_resultados.write(0, 2, "Demanda_máx")
 plan_resultados.write(0, 3, "Demanda_méd")
 plan_resultados.write(0, 4, "F_demanda")
 plan_resultados.write(0, 5, "F_carga")
+plan_resultados.write(0, 6, "Leitura da Demanda Máx")
+plan_resultados.write(0, 7, "Dia e hora da leitura")
+plan_resultados.write(0, 8, "Demanda Máx Não Div")
+plan_resultados.write(0, 9, "F_diversidade")
 
 '''Criação da lista de valores com a potência instalada'''
 S_instalada = sheet_S_instalada.col_values(colx=1,start_rowx=0)
 soma_S_instalada = 0
+soma_Demanda_máx_não_div = 0
 
 ramal_cliente = sheet_S_instalada.col_values(colx=2,start_rowx=0)
 
@@ -50,15 +56,12 @@ for i in range(1,n_clientes):
     clientes.append(novo_cliente)
 
     '''Escreve os resultados e dados do cliente na planilha de resultados'''
-    plan_resultados.write(i+offset, 0, i)
-    plan_resultados.write(i+offset, 1, novo_cliente.S_instalada)
-    plan_resultados.write(i+offset, 2, novo_cliente.Demanda_Máx)
-    plan_resultados.write(i+offset, 3, novo_cliente.Demanda_Méd)
-    plan_resultados.write(i+offset, 4, novo_cliente.F_demanda)
-    plan_resultados.write(i+offset, 5, novo_cliente.F_carga)
+
+    escrever_dados_cliente(plan_resultados,i + offset,i,novo_cliente)
 
     '''Soma a potência do cliente à potencia total do sistema'''
     soma_S_instalada += float(S_instalada[i])
+    soma_Demanda_máx_não_div += novo_cliente.Demanda_Máx
 
     '''Identifica o ramal do cliente e salva o objeto do cliente em uma lista com uma lista de clientes'''
     ramal = int(ramal_cliente[i])
@@ -80,14 +83,10 @@ for i in range(1,n_leituras):
 
 '''Criação do objeto cliente do Sistema completo'''
 Sistema = cliente(demanda_sistema[1:],soma_S_instalada)
+Sistema.Demanda_Máx_não_div = soma_Demanda_máx_não_div
+Sistema.F_diversidade = Sistema.Demanda_Máx_não_div/Sistema.Demanda_Máx
 
-plan_resultados.write(1, 0, "Sistema")
-plan_resultados.write(1, 1, Sistema.S_instalada)
-plan_resultados.write(1, 2, Sistema.Demanda_Máx)
-plan_resultados.write(1, 3, Sistema.Demanda_Méd)
-plan_resultados.write(1, 4, Sistema.F_demanda)
-plan_resultados.write(1, 5, Sistema.F_carga)
-
+escrever_dados_cliente(plan_resultados,1,'Sistema',Sistema)
 '''=============================================================================================================='''
 '''=============================================================================================================='''
 '''================================================RAMAIS========================================================'''
@@ -112,6 +111,7 @@ for ramal in range(1,8):
         '''Criação das variáveis de soma associadas à potência instalada e a soma das demandas do ramal para o intervalo'''
         soma = 0
         soma_S_instalada = 0
+        soma_Demanda_máx_não_div = 0
 
         '''Número de clientes no ramal da iteração'''
         n_clientes_ramal = len(clientes_por_ramal[ramal])
@@ -127,24 +127,21 @@ for ramal in range(1,8):
 
                 '''soma a potencia instalada do cliente ao valor total instalado no ramal'''
                 soma_S_instalada += clientes_por_ramal[ramal][id_cliente].S_instalada
+                soma_Demanda_máx_não_div += clientes_por_ramal[ramal][id_cliente].Demanda_Máx
 
         '''Inclue o valor da soma daquele intervalo à lista de demanda para o ramal'''
         demanda_ramal.append(soma)
 
     '''Criação do objeto cliente do ramal com a lista de demandas e a soma de potência instalada'''
     novo_ramal = cliente(demanda_ramal[1:],soma_S_instalada)
-
+    novo_ramal.Demanda_Máx_não_div = soma_Demanda_máx_não_div
+    novo_ramal.F_diversidade = novo_ramal.Demanda_Máx_não_div/novo_ramal.Demanda_Máx
     '''Adiciona o objeto de ramal à lista de objetos dos ramais'''
     Ramais.append(novo_ramal)
 
     '''Escreve os resultados daquele ramal na planilha de resultados'''
-    plan_resultados.write(ramal+1, 0, "Ramal " + str(ramal))
-    plan_resultados.write(ramal+1, 1, novo_ramal.S_instalada)
-    plan_resultados.write(ramal+1, 2, novo_ramal.Demanda_Máx)
-    plan_resultados.write(ramal+1, 3, novo_ramal.Demanda_Méd)
-    plan_resultados.write(ramal+1, 4, novo_ramal.F_demanda)
-    plan_resultados.write(ramal+1, 5, novo_ramal.F_carga)
 
+    escrever_dados_cliente(plan_resultados,ramal + 1,"Ramal " + str(ramal),novo_ramal)
 
 
 """Salva o arquivo do excel"""
--- Trabalho_uteis.py
+++ Trabalho_uteis.py
@@ -1,4 +1,11 @@
 
+from Trabalho_1_4 import *
+
+def leitura_To_Hora(leitura):
+    '''Alice'''
+
+    return '-'
+
 '''Classe de Objeto dos clientes'''
 class cliente:
     def __init__(self,demanda,s_instalada):
@@ -23,6 +30,24 @@ class cliente:
             self.F_carga = self.Demanda_Méd / self.Demanda_Máx
         except:
             self.F_carga = 0
+
+        self.encontrar_horario_demanda_máx()
+        # self.hora_Demanda_Máx = self.Demanda.index(str(self.Demanda_Máx))
+
+
+
+    def encontrar_horario_demanda_máx(self):
+        n_leituras = len(self.Demanda)
+        self.leitura_Demanda_Máx = 0
+        if(self.Demanda_Máx == 0):
+            self.leitura_Demanda_Máx = 11520
+
+        else:
+            for i in range(0,n_leituras):
+                if self.Demanda_Máx == float(self.Demanda[i]):
+                    self.leitura_Demanda_Máx = i + 11520
+
+        self.hora_Demanda_Máx = DataHora(self.leitura_Demanda_Máx)
 
 
 '''Criação da lista de clientes e ocupação da primeira posição para manter a numeração correta'''
@@ -49,3 +74,21 @@ def soma_sistema(demanda_intervalo):
         soma+=float(i)
 
     demanda_sistema.append(soma)
+
+def escrever_dados_cliente(plan_resultados,posição,label,cliente):
+    '''Escreve os resultados e dados do cliente na planilha de resultados'''
+    plan_resultados.write(posição, 0, label)
+    plan_resultados.write(posição, 1, cliente.S_instalada)
+    plan_resultados.write(posição, 2, cliente.Demanda_Máx)
+    plan_resultados.write(posição, 3, cliente.Demanda_Méd)
+    plan_resultados.write(posição, 4, cliente.F_demanda)
+    plan_resultados.write(posição, 5, cliente.F_carga)
+    plan_resultados.write(posição, 6, cliente.leitura_Demanda_Máx)
+    plan_resultados.write(posição, 7, cliente.hora_Demanda_Máx)
+    try:
+        plan_resultados.write(posição, 8, cliente.Demanda_Máx_não_div)
+        plan_resultados.write(posição, 9, cliente.F_diversidade)
+
+        return
+    except:
+        return
